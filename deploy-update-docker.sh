2ccedda144b89754b5bbcd70233cd41683b542f1
ubuntu <you@example.com>
2017-06-09 16:46:15 +0700

    adding -d for docker run and adding logs dir in docker build

--- deploy-update-docker.sh
+++ deploy-update-docker.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-sudo su
-
 docker pull apalahitunamanya2/customer
 
-docker run apalahitunamanya2/customer
-
-mkdir logs
-
-cd logs
-
-touch customer.log
-
-exit
+docker run -d apalahitunamanya2/customer
